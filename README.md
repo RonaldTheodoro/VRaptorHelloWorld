9b2a2ea626fb7dcc17442bc9c1d7d02d42a4d8bc
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-08-25 21:49:55 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# VRaptor Hello World
+
+Projeto basico para aplicações web usando o framework VRaptor
+
+PS: usar o servidor WildFly
---
+++ src/main/java/com/app/controller/IndexController.java
@@ -0,0 +1,11 @@
+package com.app.controller;
+
+import br.com.caelum.vraptor.Controller;
+
+@Controller
+public class IndexController {
+    
+    public void index() {
+        System.out.println("Hello World");
+    }
+}
